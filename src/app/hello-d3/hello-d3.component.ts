9e4baf4a376a3c024f16cb1e3978e419100f0a2e
Damien Sawyer <damiensawyer@gmail.com>
2017-12-04 21:30:25 +1100

    d3

--- src/app/hello-d3/hello-d3.component.ts
+++ src/app/hello-d3/hello-d3.component.ts
@@ -1,20 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import * as d3 from 'd3'
+import { Component, OnInit } from "@angular/core";
+import * as d3 from "d3";
 
 @Component({
-  selector: 'app-hello-d3',
-  templateUrl: 'hello-d3.html'
+  selector: "app-hello-d3",
+  templateUrl: "hello-d3.html"
 })
-export class HelloD3Component implements OnInit {
+export class HelloD3Component {
 
-  constructor() { 
+  constructor() {
     d3.select("p").append("svg").attr("width", 50).attr("height", 50).append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25).style("fill", "purple");
     d3.select("p").append("svg").attr("width", 50).attr("height", 50).append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25).style("fill", "blue");
     d3.select("p").append("svg").append("rect").attr("width", 125).attr("height", 750).style("fill", "red");
-
   }
-
-  ngOnInit() {
-  }
-
-}  
+}
