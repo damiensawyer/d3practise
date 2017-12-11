b0925d014f34734432d20df285e7ce762d88c446
Damien Sawyer <damiensawyer@gmail.com>
2017-12-11 06:02:44 +0000

    dots

--- src/app/hello-d3b/hello-d3b/hello-d3b.component.ts
+++ src/app/hello-d3b/hello-d3b/hello-d3b.component.ts
@@ -14,21 +14,42 @@ import { AppConfig } from '../../classes/AppConfigInterface';
 export class HelloD3bComponent implements OnInit {
   sampleChart: ChartDate[];
   message: string;
-  constructor(@Inject(APP_CONFIG) config: AppConfig, sampleChart: SampleChartService) {
+  constructor( @Inject(APP_CONFIG) config: AppConfig, sampleChart: SampleChartService) {
     this.message = config.name;
     this.sampleChart = sampleChart.simpleChart();
   }
 
   ngOnInit() {
-    d3
-      .select('p')
-      .append('svg')
-      .attr('width', 350)
-      .attr('height', 350)
-      .append('circle')
-      .attr('cx', 150)
-      .attr('cy', 125)
-      .attr('r', 25)
-      .style('fill', 'red');
+    var h = 350;
+    var w = 400;
+
+    var monthlySales = [
+      { "month": 10, "sales": 100 },
+      { "month": 20, "sales": 130 },
+      { "month": 30, "sales": 250 },
+      { "month": 40, "sales": 300 },
+      { "month": 50, "sales": 265 },
+      { "month": 60, "sales": 225 },
+      { "month": 70, "sales": 180 },
+      { "month": 80, "sales": 120 },
+      { "month": 90, "sales": 145 },
+      { "month": 100, "sales": 130 }
+    ];
+
+
+    //create our SVG
+    var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+
+    //add dots
+    var dots = svg.selectAll("circle")
+      .data(monthlySales)
+      .enter()
+      .append("circle")
+      .attr("cx", function (d) { return d.month * 3; })
+      .attr("cy", function (d) { return h - d.sales; })
+      .attr("r", 5)
+      .attr("fill", "#666666");
+
   }
 }
