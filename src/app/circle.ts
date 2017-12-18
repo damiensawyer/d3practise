60be0f0d64b5c493d889fcda00e7ada3df666071
Damien Sawyer <damiensawyer@gmail.com>
2017-12-18 03:40:49 +0000

    sam

---
+++ src/app/circle.ts
@@ -0,0 +1,15 @@
+import * as d3 from 'd3'
+import { Selection } from 'd3-selection'
+
+declare module 'd3-selection' {
+    interface Selection<GElement extends BaseType, Datum, PElement extends BaseType, PDatum> {
+        makeCircle(): Selection<EnterElement, Datum, PElement, PDatum>
+        higuys();
+    }
+}
+
+function makeCircle() {
+    console.info('Damien, coffee on you!');
+}
+
+d3.selection.prototype.makeCircle = makeCircle;
--- src/app/extensions/extensions.component.ts
+++ src/app/extensions/extensions.component.ts
@@ -2,25 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 import { BaseType } from 'd3';
 
-interface Selection<GElement extends BaseType, Datum, PElement extends BaseType, PDatum> {
-}
-// https://stackoverflow.com/questions/37297241/typescript-extend-external-javascript-library
-
-
-// .prototype.damien = () => { return "hello"; };
-
-interface MySelection extends Selection<BaseType, any, BaseType, any> {
-  damien: () => string;
-}
-
-class MySelectionConcrete<> implements MySelection {
-  constructor(source: Selection<GElement extends BaseType, Datum, PElement extends BaseType, PDatum>) {
-
-  }
-  public damien: () => "damien";
-}
-
-
 @Component({
   selector: 'app-extensions',
   templateUrl: './extensions.component.html',
@@ -33,11 +14,6 @@ export class ExtensionsComponent implements OnInit {
   h: 100;
   constructor() {
     this.data = [1];
-  }
-  makeCircle() { return null; };
-
-  toMySelection(target: Selection<BaseType, any, BaseType, any>) {
-    return target;
   }
 
   ngOnInit() {
@@ -64,16 +40,11 @@ export class ExtensionsComponent implements OnInit {
 
   }
 
-
   build2() {
     var svg = d3.selectAll("#target2")
       .attr("width", this.w)
       .attr("height", this.h);
-
-    // svg.selectAll("g")
-    //   .data(this.data)
-    //   .enter().
-
+    svg.makeCircle();
 
   }
 
