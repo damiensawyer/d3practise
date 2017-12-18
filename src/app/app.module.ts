68c11ef2cb1089511b86b84a471509a086c3a225
Damien Sawyer <damiensawyer@gmail.com>
2017-12-18 04:05:32 +0000

    comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,7 +51,7 @@ var routes: Routes = [
   bootstrap: [AppComponent]
 })
 export class AppModule {
-
+  // Need to move this into another file... but it appears to be working. Thanks SAM!! :-)
   makeCircle() {
     console.log('Damien, coffee on you!');
   }
